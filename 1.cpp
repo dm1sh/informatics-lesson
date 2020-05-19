c215458101a78e6416da10f6039d246bfa98fbba
Dm1tr1y147 <me@dmitriy.icu>
2020-05-19 21:15:19 +0500

    Треугольник паскаля, ответ — 34

---
+++ 1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int max_line()
+{
+    vector<int> arr{1};
+    int n = 1;
+    while (2 + 2 == 4)
+    {
+        arr.insert(arr.begin(), 0);
+        for (int j = 0; j < arr.size(); j++)
+        {
+            if (INT_MAX - arr[j + 1] <= arr[j])
+            {
+                return n;
+            }
+            arr[j] += arr[j + 1];
+        }
+        n++;
+    }
+}
+
+int main()
+{
+    cout << max_line() << '\n';
+    return 0;
+}
